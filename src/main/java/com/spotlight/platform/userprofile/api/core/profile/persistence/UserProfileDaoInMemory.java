c5fcf70dfbaadbcbdb0a783ef6ad2fe6bff321bb
Eugene Rabchuk <rabchuk@gmail.com>
2023-04-13 09:59:18 +0200

    New entities and temp data for test

--- src/main/java/com/spotlight/platform/userprofile/api/core/profile/persistence/UserProfileDaoInMemory.java
+++ src/main/java/com/spotlight/platform/userprofile/api/core/profile/persistence/UserProfileDaoInMemory.java
@@ -3,12 +3,20 @@ package com.spotlight.platform.userprofile.api.core.profile.persistence;
 import com.spotlight.platform.userprofile.api.model.profile.UserProfile;
 import com.spotlight.platform.userprofile.api.model.profile.primitives.UserId;
 
+import java.time.Instant;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class UserProfileDaoInMemory implements UserProfileDao {
     private final Map<UserId, UserProfile> storage = new ConcurrentHashMap<>();
+
+    {
+        UserId usedId = UserId.valueOf("1");
+        UserProfile userProfile = new UserProfile(usedId, Instant.now(), new HashMap<>());
+        storage.put(UserId.valueOf("1"), userProfile);
+    }
 
     @Override
     public Optional<UserProfile> get(UserId userId) {
---
+++ src/main/java/com/spotlight/platform/userprofile/api/model/command/primitives/CommandTypeEnum.java
@@ -0,0 +1,7 @@
+package com.spotlight.platform.userprofile.api.model.command.primitives;
+
+public enum CommandTypeEnum {
+    REPLACE,
+    INCREMENT,
+    COLLECT
+}
